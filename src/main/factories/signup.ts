784bfe3d55ed2b1ef3bcd3cbd47dd5929dc1fd52
Bruno Meneses <brunomenesessousa@gmail.com>
2021-10-27 15:56:41 -0300

    feat: ensure LogMongoRepository creates an error log on success

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,6 +4,7 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { Controller } from '@presentation/protocols'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { LogControllerDecorator } from '../decorators/log'
 
 export const makeSignUpController = (): Controller => {
@@ -13,6 +14,6 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,11 @@
-import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
-import env from "./config/env";
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const app = (await import("./config/app")).default;
+    const app = (await import('./config/app')).default
     app.listen(5050, () =>
-      console.log("Server running at http://localhost:5050")
-    );
+      console.log('Server running at http://localhost:5050')
+    )
   })
-  .catch(console.error);
+  .catch(console.error)
