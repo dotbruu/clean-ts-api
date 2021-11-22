7f8b43746b0d830f2487f5cfd848bd9beaf81747
Bruno Meneses <brunomenesessousa@gmail.com>
2021-11-22 20:07:24 -0300

    feat: add LoginValidationFactory

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,12 +1,12 @@
 import { makeValidationComposite } from './signup-validation'
-import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-filed-validation'
-import { Validation } from '../../presentation/helpers/validators/validation'
-import { CompareFieldValidation } from '../../presentation/helpers/validators/compare-filed-validation'
-import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
-import { EmailValidator } from '../../presentation/protocols'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-filed-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { CompareFieldValidation } from '../../../presentation/helpers/validators/compare-filed-validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { EmailValidator } from '../../../presentation/protocols'
 
-jest.mock('../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/main/middlewares/content-type.ts
+++ src/main/middlewares/content-type.ts
@@ -1,10 +1,11 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express'
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const contentType = (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
-  response.type("json");
-  next();
-};
+  response.type('json')
+  next()
+}
