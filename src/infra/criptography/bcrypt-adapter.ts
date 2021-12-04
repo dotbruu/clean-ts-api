19d47fb7a988139d37b2a24f4a89eece1797cce4
Bruno Meneses <brunomenesessousa@gmail.com>
2021-12-03 21:49:56 -0300

    refactor: rename Encrypter to Hasher

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,14 +1,14 @@
-import bcrypt from "bcrypt";
-import { Encrypter } from "../../data/protocols/criptography/encrypter";
+import bcrypt from 'bcrypt'
+import { Hasher } from '../../data/protocols/criptography/hasher'
 
-export class BcryptAdapter implements Encrypter {
-  private readonly salt: number;
-  constructor(salt: number) {
-    this.salt = salt;
+export class BcryptAdapter implements Hasher {
+  private readonly salt: number
+  constructor (salt: number) {
+    this.salt = salt
   }
 
-  async encrypt(value: string): Promise<string> {
-    const hash = await bcrypt.hash(value, this.salt);
-    return hash;
+  async hash (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
   }
 }
